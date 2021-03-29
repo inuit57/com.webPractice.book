dfc393556225f52f84190f718f633621916c53ee
narD <inuit57@naver.com>
2021-03-29 19:22:30 +0900

    react랑 연동 테스트

--- src/main/java/com/webPractice/book/springboot/web/HelloController.java
+++ src/main/java/com/webPractice/book/springboot/web/HelloController.java
@@ -1,7 +1,10 @@
 package com.webPractice.book.springboot.web;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
 
 @RestController
 public class HelloController {
@@ -9,5 +12,12 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello(){
         return "hello";
+    }
+
+    @GetMapping("/api/hello")
+    public String hello2(){
+        return "안녕하세요. 현재 서버시간은 "+new Date() +"입니다. \n";
+
+
     }
 }
